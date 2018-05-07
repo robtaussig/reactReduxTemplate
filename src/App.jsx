f779db16fe0a396c4720f343857ce9a7bad82955
Rob Taussig <robert.taussig@gmail.com>
2018-05-06 22:13:01 -0400

    Add styled components branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { templateAsyncAction } from './actions/appActions.js';
 import { connect } from 'react-redux';
 import './App.css';
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: white;
+  `}
+`;
 
 export class App extends React.Component {
   constructor(props) {
@@ -21,7 +36,8 @@ export class App extends React.Component {
 
     return (
       <div className={'app'}>
-
+        <Button>Normal Button</Button>
+        <Button primary>Primary Button</Button>
       </div>
     );
   }
